123d1ec415680726b4d9ad3fe73aea101d447366
Viktor Hansson <viktor.hansson@me.com>
2015-06-25 14:23:04 +0200

    Changed space tabs to tabs, for pull request

--- php/access/Admin.php
+++ php/access/Admin.php
@@ -262,7 +262,7 @@ class Admin extends Access {
 
 	private function login() {
 
-                global $dbName;
+		global $dbName;
 
 		Module::dependencies(isset($_POST['user'], $_POST['password']));
 		$session = new Session($this->database, $dbName, $this->plugins, $this->settings);
@@ -272,7 +272,7 @@ class Admin extends Access {
 
 	private function logout() {
 
-                global $dbName;
+		global $dbName;
 
 		$session = new Session($this->database, $dbName, $this->plugins, $this->settings);
 		echo $session->logout();
